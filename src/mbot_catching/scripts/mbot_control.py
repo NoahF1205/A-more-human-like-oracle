5c2aa4868d4fe4ee31c86433cb4f39a806d7c438
NoahF1205 <qfang01@tufts.edu>
2024-04-21 15:00:04 -0400

    upating on mbot cntroller

--- src/mbot_catching/scripts/mbot_control.py
+++ src/mbot_catching/scripts/mbot_control.py
@@ -5,30 +5,38 @@ import numpy as np
 from gazebo_msgs.srv import SetModelState
 from gazebo_msgs.msg import ModelState
 from gazebo_msgs.srv import GetModelState
+import tf
 
 #http://wiki.ros.org/simulator_gazebo/Tutorials/Gazebo_ROS_API#Set_and_Get_Model_Pose_and_Twist_in_Simulation_via_Service
 class MbotSim:
-    def __init__(self):
+    def __init__(self, model_name='mbot', min_r=0.2, max_r=0.6):
         rospy.init_node('mbot_sim_controller', anonymous=True)
-        self.velocity_publisher = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
-        self.odometry_subscriber = rospy.Subscriber('/odom', Odometry, self.odometry_callback)
-        self.current_odometry = None
-        self.model_name = 'mbot'
-    def odometry_callback(self, data):
-        self.current_odometry = data
 
-    def get_odometry_state(self):
-        # 确保我们已经接收到最新的里程计信息
-        while self.current_odometry is None and not rospy.is_shutdown():
-            rospy.sleep(0.1)
-        return self.current_odometry
-    
-    def set_model_state(slef, model_name, position, orientation):
+        self.model_name = model_name
+        self.min_r = min_r
+        self.max_r = max_r
+        self.velocity_publisher = rospy.Publisher(f'{self.model_name}/cmd_vel', Twist, queue_size=10)
+
+        rospy.wait_for_service('/gazebo/get_model_state')
+        self.get_state_service = rospy.ServiceProxy('/gazebo/get_model_state', GetModelState)
+        # Define direction to angle mapping (angles in radians)
+        self.directions = {
+            0: (1, 0),       # Forward
+            1: (-1, 0),      # Backward
+            2: (1, np.pi),       # Forward Left (obvious left bias)
+            3: (1, -np.pi),      # Forward Right (obvious right bias)
+            4: (-1, np.pi),      # Backward Left (obvious left bias)
+            5: (-1, -np.pi),     # Backward Right (obvious right bias)
+            6: (1, np.pi * 2),       # Faster left
+            7: (1, -np.pi * 2)       # Faster right
+        }
+
+    def set_model_state(self, position, orientation):
         rospy.wait_for_service('/gazebo/set_model_state')
         try:
             set_state = rospy.ServiceProxy('/gazebo/set_model_state', SetModelState)
             state_msg = ModelState()
-            state_msg.model_name = model_name
+            state_msg.model_name = self.model_name
             state_msg.pose.position.x = position[0]
             state_msg.pose.position.y = position[1]
             state_msg.pose.position.z = position[2]
@@ -36,42 +44,80 @@ class MbotSim:
             state_msg.pose.orientation.y = orientation[1]
             state_msg.pose.orientation.z = orientation[2]
             state_msg.pose.orientation.w = orientation[3]
-            state_msg.reference_frame = 'world'  # 或者使用其他的参考坐标系
+            state_msg.reference_frame = 'world'  
 
             resp = set_state(state_msg)
             return resp.success
         except rospy.ServiceException as e:
             print("Service call failed: %s" % e)
 
-    def get_model_state(model_name, relative_entity_name='world'):
-        rospy.wait_for_service('/gazebo/get_model_state')
+    def get_model_state(self, reference_frame='world'):
         try:
-            get_state = rospy.ServiceProxy('/gazebo/get_model_state', GetModelState)
-            resp = get_state(model_name, relative_entity_name)
+            resp = self.get_state_service(self.model_name, reference_frame)
             return resp
         except rospy.ServiceException as e:
             print("Service call failed: %s" % e)
-
+    
+    # relative to the origin postion of the mbot
+    def get_model_state_odom(self):
+        state= rospy.wait_for_message(f'{self.model_name}/odom', Odometry, timeout=None)  
+        return state
+    
     def reset(self):
-        initial_position = [0, 0, 0.1]  # 你希望小车回到的x, y, z坐标
-        initial_orientation = [0, 0, 0, 1]  # 无旋转的四元数
-        result = self.set_model_state('your_model_name', initial_position, initial_orientation)
+        # random position 
+        radius = np.random.uniform(self.min_r, self.max_r)
+        angle = np.random.uniform(0, 2 * np.pi)
+        initial_position = [radius * np.cos(angle), radius * np.sin(angle), 0]  
+        initial_orientation = [0, 0, 0, 1]  
+        result = self.set_model_state(initial_position, initial_orientation)
         if result:
-            print("Model state successfully reset.")
+            rospy.loginfo("Mbot successfully initialized.")
         else:
-            print("Failed to reset model state.")
+            rospy.logerr("Mbot initialization failed.")
 
-    def step(self, linear_velocity, angular_velocity):
-        # 发送速度命令到差速小车
-        twist = Twist()
-        twist.linear.x = linear_velocity
-        twist.angular.z = angular_velocity
-        self.velocity_publisher.publish(twist)
-        
-        # 在这里你可能想等待一小段时间让小车移动
-        rospy.sleep(1)
-        
-        # 获取小车的当前状态
-        odometry = self.get_odometry_state()
-        
-        return odometry
+    def move_mbot(self, direction, speed, duration):
+        if direction not in self.directions:
+            rospy.logerr("Invalid direction input")
+            return False
+
+        # Get the corresponding linear and angular speeds for the direction
+        linear_speed, angular_speed = self.directions[direction]
+        vel_msg = Twist()
+        vel_msg.linear.x = linear_speed * speed
+        vel_msg.angular.z = angular_speed * speed
+
+        # Get current position and direction
+        current_state = self.get_model_state()
+
+        current_x = current_state.pose.position.x
+        current_y = current_state.pose.position.y
+        current_yaw = 2 * np.arctan2(current_state.pose.orientation.z, current_state.pose.orientation.w)
+
+        # Calculate the position after movement
+        dx = linear_speed * speed * duration * np.cos(current_yaw)
+        dy = linear_speed * speed * duration * np.sin(current_yaw)
+        new_x = current_x + dx
+        new_y = current_y + dy
+
+        # Check if the new position is within the safe bounds
+        if not self.is_within_bounds(new_x, new_y):
+            rospy.logerr("Calculated position is out of safe bounds.")
+            return False
+
+        # Publish velocity messages to control robot's movement
+        rate = rospy.Rate(10)
+        end_time = rospy.Time.now() + rospy.Duration(duration)
+        while rospy.Time.now() < end_time:
+            self.velocity_publisher.publish(vel_msg)
+            rate.sleep()
+
+        # Stop the robot
+        vel_msg.linear.x = 0
+        vel_msg.angular.z = 0
+        self.velocity_publisher.publish(vel_msg)
+        return True
+
+    def is_within_bounds(self, x, y):
+        """Check if the position is within the safe bounds"""
+        radius = np.sqrt(x**2 + y**2)
+        return self.min_r <= radius <= self.max_r
--- src/mbot_catching/scripts/test.py
+++ src/mbot_catching/scripts/test.py
@@ -1,21 +1,32 @@
 from arm_control import arm_sim
+from mbot_control import MbotSim
 import numpy as np
 import time
 
-home_pose = [0.576103925704956, 0.0021510878577828407, 0.43399396538734436, 1.5707133693269406, -0.0009434863237384807, 1.5723171249353864, 0]
-action = [0.6, 0.6, 0.2, 1.5707133693269406 + np.pi/2, -0.0009434863237384807, 1.5723171249353864, 0]
+# home_pose = [0.576103925704956, 0.0021510878577828407, 0.43399396538734436, 1.5707133693269406, -0.0009434863237384807, 1.5723171249353864, 0]
+# action = [0.6, 0.6, 0.2, 1.5707133693269406 + np.pi/2, -0.0009434863237384807, 1.5723171249353864, 0]
 # action_1 = [0.3, 0.3, 0.3, 1.5799180530025816 , -0.01589837991006801, 2.61771051765151, 0]
 # action_2 = [0.3, 0.3, 0.1, 1.5799180530025816 + np.pi/2 , -0.01589837991006801, 2.61771051765151 , 0]
 # action_3 = [0.3, 0.3, 0.2, 1.5799180530025816 + np.pi/2 , -0.01589837991006801, 2.61771051765151 , 0]
 # action_4 = [0.2, 0.4, 0.3, 1.5799180530025816 + np.pi/2 , -0.01589837991006801, 2.61771051765151 , 0]
 if __name__ == '__main__':
-    arm = arm_sim()
-    arm.reset()
+    # arm = arm_sim()
+    # arm.reset()
     # print("home pose" , arm.get_cartisian_state())
-    arm.step(action)
+    # arm.step(action)
     # print(arm.step(action_1))
     # print(arm.step(action_2)) 
     # print(arm.step(action_3)) 
     # print(arm.step(action_4))
 
-            
+    mbot = MbotSim()
+    mbot.reset()
+    # print(mbot.get_model_state())
+    mbot.move_mbot(0, 0.1, 1)
+    mbot.move_mbot(1, 0.1, 1)
+    mbot.move_mbot(2, 0.1, 1)
+    mbot.move_mbot(3, 0.1, 1)
+    mbot.move_mbot(4, 0.1, 1)
+    mbot.move_mbot(5, 0.1, 1)
+    mbot.move_mbot(6, 0.1, 1)
+    mbot.move_mbot(7, 0.1, 1)    
